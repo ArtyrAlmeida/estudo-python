ba397cfd248195e5707d0de655f66ba90b5d31e4
Immanuel Victor <immanuel.victor@academico.ifpb.edu.br>
2021-08-09 18:56:16 -0300

    Exercicio 2 e ajuste de pastas

---
+++ Exercicios - Immanuel/Exercicio01/HelloWorld.py
@@ -0,0 +1,4 @@
+print ("Hello World!");
+
+msg = 'Bom dia meus caros';
+print(msg);
---
+++ Exercicios - Immanuel/Exercicio02/exercicio2.py
@@ -0,0 +1,3 @@
+nome = input("Digite seu nome: ")
+print('Seja bem vindo {}. Gostaria de lhe desejar um bom dia!'.format(nome))
+
