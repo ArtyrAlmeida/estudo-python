ae7d227ddca761e9a9135b338a55cce11470105e
Immanuel Victor <immanuel.victor@academico.ifpb.edu.br>
2021-08-10 11:32:37 -0300

    Exercicios 3 e 4 e notações da aula de primitivos

---
+++ Exercicios - Immanuel/Exercicio03/exercicio03.py
@@ -0,0 +1,11 @@
+n1 = float(input("Insira aqui um número!: "))
+n2 = float(input("Insira aqui outro número!: "))
+
+s = float(n1 + n2)
+
+sub = float(n1 - n2)
+
+mult = float(n1 * n2)
+
+print(f"a soma dos numeros {n1} e {n2} é {s}, sua subtração é {sub} e sua multiplicação é {mult}")
+
---
+++ Exercicios - Immanuel/Exercicio03/primitivos.py
@@ -0,0 +1,24 @@
+n1 = int(input("digite um numero: "))
+#print(type(n1))
+n2 = int(input("digite outro numero: "))
+#print(type(n2))
+s = int(n1+n2)
+
+x = input("digite alguma coisa: ")
+print(x.isnumeric())
+
+n = float(input("digite outro numero: "))
+print(n)
+#observando a tipagem
+
+#n3 = input("digite outro numero: ")
+#print(type(n3))
+
+#print(type(s))
+print("A soma vale:", s)
+
+#Segunda parte do código
+
+print(f"A soma de {n1} e {n2} vale {s}")
+
+
